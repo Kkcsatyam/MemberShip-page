754f604e4716d1280577b8ef01765acc7138e604
Satyam Kumar <103276124+Kkcsatyam@users.noreply.github.com>
2025-03-06 13:14:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function check() {
+  var checkBox = document.getElementById("checbox");
+  var text1 = document.getElementsByClassName("text1");
+  var text2 = document.getElementsByClassName("text2");
+
+  for (var i = 0; i < text1.length; i++) {
+    if (checkBox.checked == true) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "none";
+      text2[i].style.display = "block";
+    }
+  }
+}
+check();
+function updateCurrencySymbol() {
+  const currencySelector = document.getElementById("currency");
+  const selectedCurrency = currencySelector.value;
+  document.querySelectorAll(".text1, .text2").forEach((element) => {
+    const currentAmount = element.textContent.trim().split(" ")[1];
+    element.textContent = `${selectedCurrency} ${currentAmount}`;
+  });
+}
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  item.querySelector(".faq-question").addEventListener("click", function () {
+    item.classList.toggle("active");
+
+    // Close other open FAQs
+    faqItems.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
+    });
+  });
+});
